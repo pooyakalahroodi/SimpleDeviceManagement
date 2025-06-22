48f6f3c6a57bf5d87b11c3bb3a48fe04b8f73f1c
Lukas Rasegi <lukas.rasegi@progiton.com>
2025-06-22 22:49:29 +0200

    Code Review

--- SimpleDeviceManagement/SimpleDeviceManagementCore/src/main/java/com/progiton/trainee/simple/devicemanagement/model/enums/ActionType.java
+++ SimpleDeviceManagement/SimpleDeviceManagementCore/src/main/java/com/progiton/trainee/simple/devicemanagement/model/enums/ActionType.java
@@ -1,8 +1,19 @@
 package com.progiton.trainee.simple.devicemanagement.model.enums;
 
 public enum ActionType {
+    /**
+     * Übergabe von Gerät an User
+     */
     HANDOVER("Übergabe"),
+
+    /**
+     * Rückgabe von Gerät durch User
+     */
     RETURN("Rückgabe"),
+
+    /**
+     *
+     */
     TRANSFER("Transfer");
 
     private final String label;
